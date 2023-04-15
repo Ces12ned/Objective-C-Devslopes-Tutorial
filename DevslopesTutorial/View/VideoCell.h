ec8d88ac566b8a4c651a6bffb670d14da71e1e72
Edgar Cisneros <cisns.edg@gmail.com>
2023-04-14 23:31:48 -0600

    Table View Cell and Cell design add to the project.

---
+++ DevslopesTutorial/View/VideoCell.h
@@ -0,0 +1,16 @@
+//
+//  VideoCell.h
+//  DevslopesTutorial
+//
+//  Created by Edgar Cisneros on 14/04/23.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VideoCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
